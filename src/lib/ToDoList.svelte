c6437bc325a98c5baed340be132a20d1ae4da76f
GrungeElFz <elf0114_@hotmail.com>
2023-03-08 22:22:54 -0600

    Modified:   Added `lang='scss'` to <style> tag (src/lib/ToDoList.svelte)             Added SCSS styling to <ToDoList>

--- src/lib/ToDoList.svelte
+++ src/lib/ToDoList.svelte
@@ -103,9 +103,81 @@
 	</form>
 </div>
 
-<style>
-	.toDoList {
-		max-height: 15rem;
-		overflow: auto;
+<style lang="scss">
+	.toDoLists-wrapper {
+		background-color: rgb(25, 25, 25);
+		border: 0.1rem solid rgb(85, 85, 85);
+		border-radius: 0.5rem;
+		.no-item-text {
+			margin: 0;
+			padding: 1rem;
+			text-align: center;
+		}
+		.toDoList {
+			max-height: 15rem;
+			overflow: auto;
+			ul {
+				margin: 0;
+				padding: 1rem;
+				list-style: none;
+				li {
+					margin-bottom: 0.3rem;
+					display: flex;
+					align-items: center;
+					background-color: rgb(20, 20, 20);
+					border-radius: 0.3rem;
+					padding: 0.5rem;
+					position: relative;
+					label {
+						cursor: pointer;
+						display: flex;
+						align-items: baseline;
+						padding-right: 1rem;
+						input[type='checkbox'] {
+							cursor: pointer;
+							margin: 0 0.4rem 0 0;
+						}
+					}
+					&.completed > label {
+						opacity: 0.5;
+						text-decoration: line-through;
+					}
+					.remove-todo-button {
+						display: none;
+						border: none;
+						background: none;
+						padding: 0.3rem;
+						position: absolute;
+						right: 0.3rem;
+						cursor: pointer;
+						:global(svg) {
+							fill: rgb(100, 100, 100);
+						}
+					}
+					&:hover {
+						.remove-todo-button {
+							display: block;
+						}
+					}
+				}
+			}
+		}
+		.toDoLists-form {
+			padding: 1rem;
+			background-color: rgb(20, 20, 20);
+			border-radius: 0 0 0.5rem 0.5rem;
+			border-top: 0.1rem solid rgb(85, 85, 85);
+			display: flex;
+			flex-wrap: wrap;
+			input {
+				flex: 1;
+				color: white;
+				background-color: rgb(50, 50, 50);
+				border: 1px solid rgb(250, 135, 95);
+				border-radius: 0.7rem;
+				padding: 0.5rem;
+				margin-right: 0.5rem;
+			}
+		}
 	}
 </style>
